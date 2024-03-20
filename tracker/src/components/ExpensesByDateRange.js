3e43865123961e23d210f967cc2fb77065d9ff18
Aditi <aditi.dudalkar@gmail.com>
2024-03-20 09:42:35 +0530

    added expense list and colors to chart

--- tracker/src/components/ExpensesByDateRange.js
+++ tracker/src/components/ExpensesByDateRange.js
@@ -62,6 +62,7 @@ const ExpensesByDateRange = () => {
     if (existingChart) {
       existingChart.destroy();
     }
+    const customColors = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF8A80', '#A1887F', '#4DD0E1', '#FF5722', '#009688', '#FF1744', '#00E676', '#FF4081', '#18FFFF', '#651FFF', '#FF6E40', '#FFD740', '#7C4DFF', '#1DE9B6', '#C51162', '#FF5252', '#2962FF', '#FFD740', '#64DD17', '#FFAB00', '#FF1744', '#00B8D4', '#FF6D00', '#FFEA00', '#00C853'];
   
     new Chart(ctx, {
       type: chartType, // Use chartType state here
@@ -70,8 +71,8 @@ const ExpensesByDateRange = () => {
         datasets: [{
           label: 'Expense Categories',
           data: chartData.map(data => data.totalAmount),
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: customColors,
+          borderColor: 'black',
           borderWidth: 1
         }]
       },
@@ -116,10 +117,23 @@ const ExpensesByDateRange = () => {
         <div className="col-md-2">
           <ChartTypeDropdown onChange={handleChartTypeChange} />
         </div>
-        <div className="col-md-2 mt-3"> {/* Adjusted margin-top */}
+        <div className="col-md-2 mt-4 pt-4"> {/* Adjusted margin-top */}
           <button type="submit" className="btn btn-primary">Get Expenses</button>
         </div>
       </form>
+      <div className="mt-4 expenses-list">
+        <h5>Expenses:</h5>
+          <ul className="list-group">
+            {expenses.map((expense, index) => (
+              <li key={index} className="list-group-item">
+                <div className="d-flex justify-content-between align-items-center">
+                  <span>{expense.category}</span>
+                  <span>₹{expense.amount}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+      </div>
       <div className="mt-5">
         <canvas id="expenseChart" width="400" height="400"></canvas>
       </div>
